9d32ea81420d6c6c4fa43ae4816178b13734c449
BelikAE <BelikAE@HOME-PC>
2025-01-20 22:32:12 +1000

    Выдача

--- Module_4_14/MainWindow.xaml.cs
+++ Module_4_14/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,58 @@ using System.Windows.Shapes;
 
 namespace Module_4_14
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+    
+
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Product> products;
         public MainWindow()
         {
             InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                ProductName = "Яблоко",
+                ProductPrice = 260,
+                ProductImage = "Data\\Apple.jpg",
+                Category = ProductCategory.Food
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Манго",
+                ProductPrice = 420,
+                ProductImage = "Data\\Mango.jpg",
+                Category = ProductCategory.Food
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Апельсин",
+                ProductPrice = 360,
+                ProductImage = "Data\\Orange.jpg",
+                Category = ProductCategory.Food
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Утюг",
+                ProductPrice = 5600,
+                ProductImage = "Data\\Iron.jpg",
+                Category = ProductCategory.HouseholdAppliances
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Ноутбук",
+                ProductPrice = 86000,
+                ProductImage = "Data\\Laptop.jpg",
+                Category = ProductCategory.HouseholdAppliances
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Робот-пылесос",
+                ProductPrice = 46000,
+                ProductImage = "Data\\Robot.jpg",
+                Category = ProductCategory.HouseholdAppliances
+            });
+            listBox.ItemsSource = products;
         }
     }
 }
---
+++ Module_4_14/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_4_14
+{
+    public enum ProductCategory
+    {
+        Food,
+        HouseholdAppliances
+    }
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+        public string ProductImage { get; set; }
+        public ProductCategory Category { get; set; }
+
+    }
+}
